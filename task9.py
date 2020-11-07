6439c170c27c78f3025e3a52285e41fd02e1f9c7
00009985 <00009985wiut@gmail.com>
2020-11-07 15:51:58 +0500

    task9 done

--- task9.py
+++ task9.py
@@ -1,1 +1,10 @@
-text
+color_list = ["Red", "Green", "White", "Black"]
+indices = [0, 3]
+selected_elements = []
+for index in indices:
+    selected_elements.append(color_list[index])
+
+print(selected_elements)
+
+
+
