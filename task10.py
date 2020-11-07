72c91f78177d81ef3c9629dfdbd4bf1678074c83
00009985 <00009985wiut@gmail.com>
2020-11-07 17:59:49 +0500

    task10 done

--- task10.py
+++ task10.py
@@ -1,1 +1,13 @@
-test
+def pattern(n):
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print("* ", end="")
+        print("")
+    for i in range(n, 0, -1):
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("")
+
+
+pattern(4)
+print("*")
